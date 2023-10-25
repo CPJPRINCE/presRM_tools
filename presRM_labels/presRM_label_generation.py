db818809ee5b4b830a7f6976cd2a0b065b05ea25
CPJPRINCE <c.pj.prince@gmail.com>
2023-10-25 08:53:57 +0100

    label generation v2.0 GUI

--- presRM_labels/presRM_label_generation.py
+++ presRM_labels/presRM_label_generation.py
@@ -1,8 +1,9 @@
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.lib.units import inch
 from reportlab.platypus import Paragraph,KeepInFrame, Frame
-from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.graphics.barcode import code39,code93,code128
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib.utils import ImageReader
+from reportlab.graphics.barcode import code128
 from pyPreservica import *
 import argparse
 from datetime import datetime
@@ -15,25 +16,27 @@ except:
     s_flag = False
 
 def login_preservica(username,password,server="unilever.preservica.com"):
+    if username == None or password == None:
+        print('Username or Password is set to none... Please address...')
+        raise SystemExit()
     content = ContentAPI(username,password,server=server)
     entity = EntityAPI(username,password,server=server)
+    print('Successfully logged into Preservica')
     return content,entity
 
 class LabelGenerator:
-    def __init__(self,username=None,password=None,boxref=None,boxonly=False,filesonly=False,toplevel="2591783b-725e-4373-9ac1-64e73541e7fa",output=os.getcwd(),combine=False,barcode_flag=False):
-        self.username = username
-        self.password = password
-        self.boxref = self.check_boxref(boxref)
+    def __init__(self,content,entity,boxref=None,boxonly=False,filesonly=False,toplevel="2591783b-725e-4373-9ac1-64e73541e7fa",output=os.path.dirname(__file__),combine=False,barcode_flag=False):
+        self.content = content
+        self.entity = entity
+        self.box_list = self.check_boxref(boxref)
         self.box_only = boxonly
         self.files_only = filesonly
         self.combine = combine
         self.top_level = toplevel
         self.objtype = "Box"
         self.output_directory = output
-        self.content,self.entity = login_preservica(self.username,self.password)        
-        self.search_list = self.search_preservica(self.top_level,self.boxref)
         self.barcode_flag = barcode_flag
-
+                
     def search_preservica(self,top_level,search_ref):
         filters = {"xip.parent_hierarchy": top_level,"xip.title":search_ref,"rm.objtype":self.objtype,"rm.location":"*","rm.weight":"*"}
         search = list(self.content.search_index_filter_list(query="%",filter_values=filters))
@@ -54,163 +57,13 @@ class LabelGenerator:
             elif file.endswith('.xlsx'):
                 import pandas as pd
                 df = pd.read_excel(os.path.abspath(file),header=None)
-                rlist = df[0].values.tolist()
-                boxref = rlist
+                boxref = df[0].values.tolist()
                 break
             else: pass
         return boxref
-
-    def main(self):
-        if self.combine: 
-            box_combine_list = []
-            file_combine_list = []
-        if self.files_only and self.box_only: print('Both Box Only and Files Only options are set... Please choose only one...'); raise SystemExit()
-        for hit in self.search_list:
-            if not list(self.search_list): print(f'\n***\nNo Matches were found in Preservica for: {self.boxref}\n***\n'); continue
-            dept_lookup = self.entity.folder(hit.get('xip.parent_hierarchy')[0].split(" ")[-5]).title
-            box = BoxLabel(self,location=hit.get('rm.location'),weight=hit.get('rm.weight'),department=dept_lookup)
-            box.box_label_generation()
-            files = FileLabels(box,self,hit.get('xip.reference'))
-            files.file_label_loop()
-
-class BoxLabel(LabelGenerator):
-    def __init__(self, LABEL, file_name="_Box Label.pdf", location=None, department=None, weight=None):
-        self.boxref = LABEL.boxref
-        self.file_name = self.boxref.replace("/","-") + file_name
-        self.file_output = os.path.join(LABEL.output_directory,self.file_name)
-        self.location = location
-        self.department = department
-        self.weight = weight
-        self.barcode = self.boxref
-        self.barcode_flag = LABEL.barcode_flag
-        self.styleNormal = ParagraphStyle('font',fontName="Helvetica",fontSize=18,leading=16)
-        self.styleLarge = ParagraphStyle('font',fontName="Helvetica",fontSize=36,leading=30)
-        self.styleSmall = ParagraphStyle('font',fontName="Helvetica",fontSize=16,leading=14)
-
-    def box_label_generation(self):
-        if self.location: self.location = f"Location: {self.location}"
-        if self.weight: self.weight = f"Weight: {self.weight} kg"
-        C = Canvas(self.file_output)
-        C.setPageSize((4*inch,4*inch))
-        f1 = Frame(0.25*inch,3.0*inch,3.5*inch,0.75*inch,showBoundary=0)
-        f2 = Frame(0.25*inch,2.25*inch,3.5*inch,0.75*inch,showBoundary=0)
-        f3 = Frame(0.25*inch,1.5*inch,3.5*inch,0.75*inch,showBoundary=1)
-        f4 = Frame(0.25*inch,0.75*inch,3.5*inch,0.75*inch,showBoundary=0)
-        if self.barcode_flag:
-            bcode = code128.Code128(self.barcode,barHeight=(0.5*inch),humanReadable=True)
-            bcode.drawOn(C,0.25*inch,0.25*inch)
-        f1.addFromList([add_to_para_box(self.boxref,self.styleLarge)],C)
-        f2.addFromList([add_to_para_box(self.department,self.styleNormal)],C)
-        f3.addFromList([add_to_para_box(self.location,self.styleNormal)],C)
-        f4.addFromList([add_to_para_box(self.weight,self.styleNormal)],C)
-        C.save()
-        print(f"Box Label Generated to: {self.file_output}")
     
-class FileLabels(BoxLabel,LabelGenerator):
-    def __init__(self,BOXLABEL,LABEL,xip_reference,file_name="_File Labels.pdf"):
-        self.boxref = LABEL.boxref
-        self.xipref = xip_reference
-        self.file_name = self.boxref.replace('/','-') + file_name 
-        self.file_output = os.path.join(LABEL.output_directory,self.file_name)
-        self.styleNormal = ParagraphStyle('font',fontName="Helvetica",fontSize=18,leading=16)
-        self.styleLarge = ParagraphStyle('font',fontName="Helvetica",fontSize=36,leading=30)
-        self.styleSmall = ParagraphStyle('font',fontName="Helvetica",fontSize=16,leading=14)
-        self.department = BOXLABEL.department
-        self.location = BOXLABEL.location
-        self.barcode_flag = LABEL.barcode_flag
-        self.entity = LABEL.entity
-    
-    def file_label_loop(self):
-        C = Canvas(self.file_output)
-        children = list(self.entity.descendants(self.entity.folder(self.xipref)))
-        children.sort(key=lambda x: x.title)
-        for child in children:
-            e = self.entity.entity(child.entity_type,child.reference)
-            self.item_presref = e.reference
-            self.item_title = e.title
-            self.item_desc = e.description
-            self.file_label_generation(C)
-        C.save()
-        print(f"File Labels Generated to: {self.file_output}")
-    
-    def file_label_generation(self,C):
-        if self.location: location = f"Location: {self.location}"
-        C.setPageSize((4*inch,2.25*inch))
-        f1 = Frame(0.25*inch,1.7*inch,3.5*inch,0.4*inch,showBoundary=0)
-        f2 = Frame(0.25*inch,1.3*inch,2.5*inch,0.4*inch,showBoundary=0)
-        f3 = Frame(0.25*inch,0.9*inch,2.5*inch,0.4*inch,showBoundary=0)
-        f4 = Frame(0.25*inch,0.55*inch,2.5*inch,0.35*inch,showBoundary=0)
-        f1.addFromList([add_to_para_file(self.item_title,self.styleNormal)],C)
-        f2.addFromList([add_to_para_file(self.item_desc,self.styleSmall)],C)
-        f3.addFromList([add_to_para_file(self.department,self.styleSmall)],C)
-        f4.addFromList([add_to_para_file(location,self.styleSmall)],C)
-        if self.barcode_flag:
-            bcode = code128.Code128(self.item_presref,barHeight=(0.25*inch),barWidth=(0.0080*inch),humanReadable=True)
-            bcode.drawOn(C,0.15*inch,0.25*inch)
-        C.showPage()
-        return C
-
-def add_to_para_box(string,style):
-    para = [Paragraph(string,style)]
-    para_if = KeepInFrame(3.5*inch,0.75*inch,para)
-    return para_if
-
-def add_to_para_file(string,style):
-    para = [Paragraph(string,style)]
-    para_if = KeepInFrame(2.5*inch,0.3*inch,para)
-    return para_if
-
-def parse_args():
-    parser = argparse.ArgumentParser(prog="Label Generator",description="Generates Box / File Labels for RM Preservica")
-    parser.add_argument('boxref',nargs='+')
-    parser.add_argument('-b','--box-only',action='store_true')
-    parser.add_argument('-f','--files-only',action='store_true')
-    parser.add_argument('--top-level',nargs='?', default="de7eaf74-7ad3-4c9b-bab6-ae21008185f0")
-    parser.add_argument('-o', '--output', nargs='?',default=os.getcwd())
-    parser.add_argument('-c', '--combine',action='store_true')
-    if s_flag:
-        try:
-            args.username = secret.username
-            args.password = secret.password
-        except:
-            parser.add_argument('-u','--username',nargs='?')
-            parser.add_argument('-p','--password',nargs='?')
-    args = parser.parse_args()
-    return args
-
-if __name__ == "__main__":
-    starttime = datetime.now()
-    print(starttime)
-    args = parse_args()
-    LabelGenerator(username=args.username,password=args.password,boxref=args.boxref,boxonly=args.box_only,filesonly=args.files_only,toplevel=args.top_level).main()
-    raise SystemExit()
-
-raise SystemExit()
-for boxref in args.boxref:
-    label = LabelGenerator(boxref,boxonly=args.box_only,filesonly=args.files_only,toplevel=args.top_level,output=args.output)
-    if not list(search): print(f'\n***\nNo Matches were found in Preservica for: {label.boxref}\n***\n'); continue
-    for hit in search:
-        presref = hit.get('xip.reference')
-        boxref = hit.get('xip.title')
-        boxweight = hit.get('rm.weight')
-        boxlocation = str(hit.get('rm.location'))
-        dept_lookup = hit.get('xip.parent_hierarchy')[0].split(" ")[-5]
-        boxdept = entity.folder(dept_lookup).title
-        if label.files_only:
-            print('File only selected, skipping File Creation')
-            pass
-        else:
-            box_label_generation(boxref,dept=boxdept,location=boxlocation,weight=boxweight,barcode=None)
-            if args.combine: box_combine_list.append(fpath)
-        if label.box_only:
-            print('Box only selected, skipping File Creation')
-            pass
-        else: 
-            if args.combine: file_combine_list.append(fpath)
-            print(f'File Label Generated to: {fpath}')
-
-if args.combine:
-    def combine_pdf(pdf_list,output_path):
+    def combine_pdf(self,pdf_list,output_path):
+        print(f'Combined PDFs to: {output_path}')
         combiner = PdfWriter()
         for pdf in pdf_list:
             if os.path.exists(pdf):
@@ -221,17 +74,166 @@ if args.combine:
                 print(f'Error: {e}')
                 print('Possibly Multiple Matches in Preservica Search ')
         combiner.write(output_path)
-        print(f'Combined PDFs to: {output_path}')
         combiner.close()
-    print('Combining PDFs...')
-    if box_combine_list:
-        combine_pdf(box_combine_list,os.path.join(label.output,"Combined_Box Labels.pdf"))
-    if file_combine_list:
-        combine_pdf(file_combine_list,os.path.join(label.output,"Combined_File Labels.pdf"))
-    
 
-finishtime = datetime.now() - starttime
-print(f'Complete! This process took: {finishtime}')
-sleep(5)
-print('Now go print them labels!!')
-sleep(2)
+    def main(self):
+        os.chdir(os.path.dirname(__file__))
+        if self.combine: 
+            box_combine_list = []
+            file_combine_list = []
+        if self.files_only and self.box_only: print('Both Box Only and Files Only options are set... Please choose only one...'); raise SystemExit()
+        for box in self.box_list:
+            box_label = BoxLabel(self,box)
+            if box_label.match_flag:
+                if not self.files_only:
+                    box_output = box_label.box_label_generation()
+                    if self.combine: box_combine_list.append(box_output)
+                if not self.box_only:
+                    files = FileLabels(box_label,self)
+                    file_output = files.file_label_loop()
+                    if self.combine: file_combine_list.append(file_output)
+            else: pass
+        if self.combine:
+            if box_combine_list:
+                self.combine_pdf(box_combine_list,os.path.join(self.output_directory,"Combined_Box Labels.pdf"))
+            if file_combine_list:
+                self.combine_pdf(file_combine_list,os.path.join(self.output_directory,"Combined_File Labels.pdf"))
+
+class BoxLabel(LabelGenerator):
+    def __init__(self, LABEL, box, file_name="_Box Label.pdf"):
+        self.entity = LABEL.entity
+        self.barcode_flag = LABEL.barcode_flag
+        self.combine = LABEL.combine
+        self.boxref = box
+        self.file_name = self.boxref.replace("/","-") + file_name
+        self.file_output = os.path.join(LABEL.output_directory,self.file_name)
+        self.styleNormal = ParagraphStyle('font',fontName="Helvetica",fontSize=24,leading=24,valign='middle')
+        self.styleLarge = ParagraphStyle('font',fontName="Helvetica",fontSize=36,leading=34)
+        self.styleSmall = ParagraphStyle('font',fontName="Helvetica",fontSize=16,leading=14)
+        self.box_search = LABEL.search_preservica(LABEL.top_level,self.boxref)
+        self.uarmimage = os.path.relpath("./uarm.jpg")
+        self.match_flag = self.match_check() 
+        self.box_label_values()
+        
+    def match_check(self):
+        if not list(self.box_search):print(f'\n***\nNo Matches were found in Preservica for: {self.boxref}\n***\n');return False
+        else: return True
+    
+    def box_label_values(self):
+        for x in self.box_search:
+            self.xipref = x.get('xip.reference')
+            self.location = x.get('rm.location')
+            self.weight = x.get('rm.weight')
+            dept_lookup = x.get('xip.parent_hierarchy')[0].split(" ")[-5]
+            self.department = self.entity.folder(dept_lookup).title
+    
+    def box_label_generation(self):
+        if self.location: location = f"Location: {self.location}"
+        if self.weight: weight = f"Weight: {self.weight} kg"
+        C = Canvas(self.file_output)
+        C.setPageSize((4*inch,4*inch))
+        f1 = Frame(0.2*inch,2.9*inch,2.8*inch,0.9*inch,showBoundary=0)
+        f2 = Frame(0.2*inch,1.1*inch,3.6*inch,1.8*inch,showBoundary=0)
+        f3 = Frame(0.2*inch,0.65*inch,1.8*inch,0.45*inch,showBoundary=1)
+        f4 = Frame(0.2*inch,0.2*inch,1.8*inch,0.45*inch,showBoundary=1)
+        f5 = Frame(2.0*inch,0.65*inch,1.8*inch,0.45*inch,showBoundary=1)
+        f6 = Frame(2.0*inch,0.2*inch,1.8*inch,0.45*inch,showBoundary=1)
+        if self.barcode_flag:
+            bcode = code128.Code128(self.barcode,barHeight=(0.5*inch),humanReadable=True)
+            bcode.drawOn(C,0.25*inch,0.25*inch)
+        f1.addFromList([add_to_para_box(self.boxref,self.styleLarge)],C)
+        f2.addFromList([add_to_para_box(self.department,self.styleNormal)],C)
+        f3.addFromList([add_to_para_box(location,self.styleNormal)],C)
+        f4.addFromList([add_to_para_box(weight,self.styleNormal)],C)
+        try: C.drawInlineImage(self.uarmimage,2.8*inch,0.4*inch,width=1.0*inch,preserveAspectRatio=True)
+        except: pass
+        C.save()
+        print(f"Box Label Generated to: {self.file_output}")
+        return self.file_output
+        
+class FileLabels(BoxLabel,LabelGenerator):
+    def __init__(self,BOXLABEL,LABEL,file_name="_File Labels.pdf"):
+        self.file_name = BOXLABEL.boxref.replace('/','-') + file_name 
+        self.file_output = os.path.join(LABEL.output_directory,self.file_name)
+        self.styleNormal = ParagraphStyle('font',fontName="Helvetica",fontSize=20,leading=18)
+        self.styleLarge = ParagraphStyle('font',fontName="Helvetica",fontSize=28,leading=26)
+        self.styleSmall = ParagraphStyle('font',fontName="Helvetica",fontSize=16,leading=14)
+        self.entity = LABEL.entity
+        self.barcode_flag = LABEL.barcode_flag
+        self.xipref = BOXLABEL.xipref
+        self.location = BOXLABEL.location
+        self.department = BOXLABEL.department
+        self.uarmimage = BOXLABEL.uarmimage
+        
+    def file_label_loop(self):
+        C = Canvas(self.file_output)
+        children = list(self.entity.descendants(self.entity.folder(self.xipref)))
+        children.sort(key=lambda x: "-".join([str(x).zfill(5) for x in str(x.title).split("/")]))
+        #children.sort(key=lambda x: [str(x.title).split()])
+        for child in children:
+            e = self.entity.entity(child.entity_type,child.reference)
+            self.item_presref = e.reference
+            self.item_title = e.title
+            self.item_desc = e.description
+            self.file_label_generation(C)
+        C.save()
+        print(f"File Labels Generated to: {self.file_output}")
+        return self.file_output
+    
+    def file_label_generation(self,C):
+        if self.location: location = f"Location: {self.location}"
+        C.setPageSize((4*inch,2.25*inch))
+        f1 = Frame(0.25*inch,1.625*inch,3.0*inch,0.5*inch,showBoundary=0)
+        f2 = Frame(0.25*inch,1.125*inch,3.5*inch,0.5*inch,showBoundary=0)
+        f3 = Frame(0.25*inch,0.625*inch,3.5*inch,0.5*inch,showBoundary=0)
+        f4 = Frame(0.25*inch,0.125*inch,3.5*inch,0.5*inch,showBoundary=0)
+        f1.addFromList([add_to_para_file(self.item_title,self.styleNormal)],C)
+        f2.addFromList([add_to_para_file(self.item_desc,self.styleSmall)],C)
+        f3.addFromList([add_to_para_file(self.department,self.styleSmall)],C)
+        f4.addFromList([add_to_para_file(location,self.styleSmall)],C)
+        if self.barcode_flag:
+            bcode = code128.Code128(self.item_presref,barHeight=(0.25*inch),barWidth=(0.0080*inch),humanReadable=True)
+            bcode.drawOn(C,0.15*inch,0.25*inch)
+        try: C.drawInlineImage(self.uarmimage,3.0*inch,-1.1*inch,width=0.75*inch,preserveAspectRatio=True) 
+        except: pass            
+        C.showPage()
+        return C
+
+def add_to_para_box(string,style):
+    para = [Paragraph(string,style)]
+    para_if = KeepInFrame(3.5*inch,0.9*inch,para)
+    return para_if
+
+def add_to_para_file(string,style):
+    para = [Paragraph(string,style)]
+    para_if = KeepInFrame(3.5*inch,0.5*inch,para)
+    return para_if
+
+def parse_args():
+    parser = argparse.ArgumentParser(prog="Label Generator",description="Generates Box / File Labels for RM Preservica")
+    parser.add_argument('boxref',nargs='+')
+    parser.add_argument('-b','--box-only',action='store_true')
+    parser.add_argument('-f','--files-only',action='store_true')
+    parser.add_argument('--top-level',nargs='?', default="de7eaf74-7ad3-4c9b-bab6-ae21008185f0")
+    parser.add_argument('-o', '--output', nargs='?',default=os.path.dirname(__file__))
+    parser.add_argument('-c', '--combine',action='store_true')
+    parser.add_argument('-u','--username',nargs='?')
+    parser.add_argument('-p','--password',nargs='?')
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    starttime = datetime.now()
+    print(starttime)
+    args = parse_args()
+    if s_flag:
+        print(f'Secret file detected, utilising credentials for: {secret.username}')
+        user = secret.username
+        passwd = secret.password
+    else:
+        user = args.username
+        passwd = args.password    
+    content,entity = login_preservica(user,passwd)
+    LabelGenerator(content=content,entity=entity,boxref=args.boxref,boxonly=args.box_only,combine=args.combine,filesonly=args.files_only,toplevel=args.top_level,output=args.output).main()
+    finishtime = datetime.now() - starttime
+    f'Complete! This process took: {finishtime}'
---
+++ presRM_labels/presRM_label_gui.py
@@ -0,0 +1,148 @@
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import filedialog
+from presRM_label_generation import LabelGenerator, login_preservica
+from time import sleep
+import os
+import shlex
+try:
+    import secret
+    s_flag = True
+except ImportError:
+    s_flag = False
+
+
+def browse_button_output():
+    file = filedialog.askdirectory()
+    output.set(file)
+    
+def browse_button_boxref():
+    file = filedialog.askopenfile(mode='r',filetypes=[('Excel Files','*.xlsx'),('CSV Files','*.csv')])
+    boxref.set(file)
+    
+def try_login_pres(user,passwd,fail_count=0):
+    try:
+        content,entity = login_preservica(username=user,password=passwd)
+    except Exception as e:
+        if e.args[0] in {401}:    
+            print('Error signing in... Please try again...')
+            if fail_count == 2: print('This is your last chance...')
+            elif fail_count == 3: print('YOU. ARE. DONE.'); sleep(10); raise SystemExit()
+            fail_count += 1
+            content, entity = try_login_pres(fail_count)
+        elif e.args[0] in {404,501}:
+            print('Preservica is having a moment, please try again later')
+        else:
+            print('An unknown error occured... Panic!')
+            print(e)
+            sleep(5)        
+    return content,entity
+    
+def main():
+    if s_flag:
+        user = secret.username
+        passwd = secret.password
+    else:
+        user = user_entry.get()
+        passwd = pass_entry.get()
+    content,entity = try_login_pres(user=user,passwd=passwd)
+    boxref = shlex.split(boxref_entry.get())
+    output=output_entry.get()
+    combine=bool(combine_val.get())
+    filesonly=bool(filesonly_val.get())
+    boxonly = bool(boxonly_val.get())
+    toplevel = toplevel_entry.get()
+    print(boxref,output,combine,filesonly,boxonly,toplevel)
+    LabelGenerator(content=content,entity=entity,boxref=boxref,output=output,boxonly=boxonly,combine=combine,filesonly=filesonly,toplevel=toplevel).main()
+    
+window = Tk()
+window.title("UARM Label Generator")
+for c in range(3):
+    window.columnconfigure(c,weight=1)
+for r in range(4):
+    window.rowconfigure(r,weight=1)
+
+master_frame = Frame(master=window,width=50,height=100)
+master_frame.pack()
+
+cred_frame = Frame(master=master_frame,width=50,height=50)
+cred_frame.pack()
+subframe1 = Frame(master=cred_frame,width=25,height=25)
+subframe1.pack(side=LEFT)
+subframe2 = Frame(master=cred_frame,width=25,height=25)
+subframe2.pack(side=RIGHT)
+if s_flag:
+    user_label = Label(master=master_frame, text="Username & Password assigned by Secrets Files")
+    user_label.pack()
+else:
+    user_label = Label(master=subframe1,text="Username")
+    user_entry = Entry(master=subframe1,width=25)
+    pass_label = Label(master=subframe2,text="Password")
+    pass_entry = Entry(master=subframe2,width=25)
+    user_label.pack()
+    user_entry.pack()
+    pass_label.pack()
+    pass_entry.pack()
+    
+box_frame = Frame(master=master_frame,width=50,height=50)
+box_frame.pack()
+bsubframe1 = Frame(master=box_frame,width=25,height=25)
+bsubframe1.pack(side=LEFT)
+bsubframe2 = Frame(master=box_frame,width=25,height=25)
+bsubframe2.pack(side=RIGHT)
+
+boxref = StringVar(master=subframe1)
+boxref_label = Label(master=bsubframe1,text="Box Reference")
+boxref_entry = Entry(master=bsubframe1,textvariable=boxref,width=25)
+boxref_label.pack()
+boxref_entry.pack()
+boxref_button = Button(master=bsubframe2,text="Browse",command=browse_button_boxref)
+boxref_button.pack()
+
+output_frame = Frame(master=master_frame,width=50,height=50)
+output_frame.pack()
+osubframe1 = Frame(master=output_frame,width=25,height=25)
+osubframe1.pack(side=LEFT)
+osubframe2 = Frame(master=output_frame,width=25,height=25)
+osubframe2.pack(side=RIGHT)
+
+output = StringVar(master=osubframe1,value=str(os.getcwd()))
+output_label = Label(master=osubframe1,text="Output Directory")
+output_entry = Entry(master=osubframe1,textvariable=output,width=25)
+output_label.pack()
+output_entry.pack()
+output_button = Button(master=osubframe2,text="Browse",command=browse_button_output)
+output_button.pack()
+
+output_frame.pack()
+
+toplevel_frame = Frame(master=window)
+toplevel_val = StringVar(master=toplevel_frame,value="de7eaf74-7ad3-4c9b-bab6-ae21008185f0")
+toplevel_label = Label(master=toplevel_frame,text="Top Level for Search")
+toplevel_entry = Entry(master=toplevel_frame,textvariable=toplevel_val,state=DISABLED)
+toplevel_label.pack()
+toplevel_entry.pack()
+toplevel_frame.pack()
+
+checks_frame = Frame(master=window)
+
+filesonly_val = IntVar(master=checks_frame, value=0)
+filesonly_check = Checkbutton(master=checks_frame,text="Files Only",variable=filesonly_val)
+filesonly_check.pack()
+boxonly_val = IntVar(value=0)
+boxonly_check = Checkbutton(master=checks_frame,text="Box Only",variable=boxonly_val)
+boxonly_check.pack()
+combine_val = IntVar(value=1)
+combine_check = Checkbutton(master=checks_frame,text="Combine",variable=combine_val)
+combine_check.pack()
+
+checks_frame.pack()
+
+run_frame = Frame(master=window)
+run_button = Button(master=run_frame,text="Generate Label",command=main)
+run_button.pack()
+run_frame.pack()
+
+window.mainloop()
+
+raise SystemExit()
