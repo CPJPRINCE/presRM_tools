ec2ff2dc68dc594bfc6d3cc9458fa2a3cb49013f
CPJPRINCE <c.pj.prince@gmail.com>
2023-10-25 08:52:57 +0100

    retention report finalisation v1.0

--- presRM_reports/presRM_rentention.py
+++ presRM_reports/presRM_rentention.py
@@ -1,45 +1,22 @@
 from pyPreservica import *
-import secret
 import pandas as pd
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+try:
+    import secret
+except:
+    pass
+import numpy as np
 
-
-entity = EntityAPI(username={secret.username},password={secret.password}, \
-                    server="unilever.preservica.com")
-
-retention = RetentionAPI(username={secret.username},password={secret.password}, \
-                    server="unilever.preservica.com")        
-
-content = ContentAPI(username={secret.username},password={secret.password}, \
-                    server="unilever.preservica.com")
-
-def parent_return(ref,io):
-    if io == "IO": x = entity.asset(ref)
-    elif io == "SO": x = entity.folder(ref)
-    else: print("Error?")
-    while x.parent is not None:
-        f = entity.folder(x.parent)
-        parent_name = f.title
-    return parent_name
-
-def path_return(ref,io,split_flag):
-    if io == "IO": x = entity.asset(ref)
-    elif io == "SO": x = entity.folder(ref)
-    else: print("Error?")
-    fpath = x.title
-    f = entity.folder(x.parent)
-    parent_name = f.title    
-    while f.parent is not None:        
-        fpath = f.title + ":::" + fpath
-        f = entity.folder(f.parent)    
-    if split_flag:
-        fpath = fpath.rsplit(":::",maxsplit=1)[0]
-        fpath = fpath.split(":::",maxsplit=3)[-1]
-    return fpath,parent_name
+def login_preservica(user,passwd,sever="unilever.preservica.com"):
+    entity = EntityAPI(username=user,password=passwd, server=sever)
+    retention = RetentionAPI(username=user,password=passwd, server=sever)        
+    content = ContentAPI(username=user,password=passwd, server="unilever.preservica.com")
+    print('Successfully logged into Preservica')
+    return entity,retention,content
 
 class RetentionReport():
-    def __init__(self,REF="bf614a5d-10db-4ff8-addb-2c913b3149eb",OUTPUT_DIR=os.getcwd(),REPORT_TYPE="ALL",EXPIRED_FLAG=True,PATH_FLAG=False,SPLIT_FLAG=True):
+    def __init__(self,entity=None, content=None, retention=None, REF="bf614a5d-10db-4ff8-addb-2c913b3149eb",OUTPUT_DIR=os.getcwd(),REPORT_TYPE="ALL",EXPIRED_FLAG=True,PATH_FLAG=False,SPLIT_FLAG=True):
          self.reference = REF 
          self.output_dir = os.path.abspath(OUTPUT_DIR)
          self.report_type = REPORT_TYPE.upper()
@@ -47,6 +24,9 @@ class RetentionReport():
          self.expired_flag = EXPIRED_FLAG
          self.path_flag = PATH_FLAG
          self.split_flag = SPLIT_FLAG
+         self.entity = entity
+         self.content = content
+         self.retention = retention
          self.metadata_fields = {
             "xip.reference": "",
             "xip.title": "*",
@@ -61,19 +41,48 @@ class RetentionReport():
             "rm.location": "",
             "rm.defaultlocation": "",
             "rm.client": "",
-            "rm.coverdates": "",
+            "rm.coverdate": "",
             "rm.statusdate": ""}
+    def parent_return(self,ref,io):
+        if io == "IO": x = self.entity.asset(ref)
+        elif io == "SO": x = self.entity.folder(ref)
+        else: print("Error?")
+        while x.parent is not None:
+            f = self.entity.folder(x.parent)
+            parent_name = f.title
+            parent_desc = f.description
+        return parent_name,parent_desc
 
-    def report_expired(self, assignments,dict_retention):
+    def path_return(self,ref,io,split_flag):
+        if io == "IO": x = self.entity.asset(ref)
+        elif io == "SO": x = self.entity.folder(ref)
+        else: print("Error?")
+        fpath = x.title
+        f = self.entity.folder(x.parent)
+        parent_name = f.title    
+        parent_desc = f.description
+        while f.parent is not None:        
+            fpath = f.title + ":::" + fpath
+            f = self.entity.folder(f.parent)    
+        if split_flag:
+            fpath = fpath.rsplit(":::",maxsplit=1)[0]
+            fpath = fpath.split(":::",maxsplit=3)[-1]
+        return fpath,parent_name,parent_desc
+    
+    def report_expired(self, assignments,policy,dict_retention):
         for a in assignments:
-            if a.expired: dict_retention.update({'Expired':a.expired})
+            if a.expired:
+                if policy.period_unit == "MONTH": p_unit = {'months':int(policy.period)}
+                else: p_unit = {'years':int(policy.period)}
+                due_date = dict_retention.get('Status_Date') + relativedelta(**p_unit)
+                dict_retention.update({'Expired': a.expired,'Due_Date':due_date})            
             else: pass
             
     def report_not_expired(self, assignments,policy,dict_retention):
         for a in assignments:
-            if policy.period_unit == "MONTH": p_unit = {'months'}
-            else: p_unit = {'years'}
-            due_date = datetime.strptime(dict_retention.get('Status_Date'),"%Y-%m-%dT%H:%M:%SZ") + relativedelta(**{p_unit} + int(policy.period))
+            if policy.period_unit == "MONTH": p_unit = {'months':int(policy.period)}
+            else: p_unit = {'years':int(policy.period)}
+            due_date = dict_retention.get('Status_Date') + relativedelta(**p_unit)
             dict_retention.update({'Expired': a.expired,'Period':policy.period,'Period_Unit': policy.period_unit,'Due_Date':due_date})
 
     def report_mode_select(self):
@@ -84,20 +93,31 @@ class RetentionReport():
         else: print('Invalid mode, raising SystemExit'); raise SystemExit()
         return mode
     
+    def export_xl(self,df,boxdf,output_file):
+        try:
+            with pd.ExcelWriter(output_file,engine='auto',mode='w') as writer:
+                df.to_excel(writer,index=False,sheet_name="Master")
+                boxdf.to_excel(writer,index=False,sheet_name="Box Summary")
+            print(f'File Saved to: {output_file}')
+        except:
+            print(f'Save failed... Please ensure file is closed: {output_file}')
+            time.sleep(10)
+            self.export_xl(df,boxdf,output_file)
+    
     def main(self):
-
         print('Initiating search...')
         list_retention = []
-        content.search_callback(content.ReportProgressCallBack())
-        report_search = list(content.search_index_filter_list(query="%", filter_values=self.metadata_fields))
+        self.content.search_callback(self.content.ReportProgressCallBack())
+        report_search = list(self.content.search_index_filter_list(query="%", filter_values=self.metadata_fields))
         t = len(report_search)
+        print()
         print(f"Total Results: {str(t)}")
         if not t == 0:
             c = 0
             for f in report_search:
                 c += 1
                 print(f"Retrieving Expiration Status: {c} / {t}", end="\r")
-                asset = entity.asset(f['xip.reference'])
+                asset = self.entity.asset(f['xip.reference'])
                 dict_retention = {'Reference': f.get('xip.reference'),
                                 'Document_Type': f.get('xip.document_type'),                               
                                 'Title': f.get('xip.title'),
@@ -108,55 +128,63 @@ class RetentionReport():
                                 'Default_Location': f.get('rm.defaultlocation'),
                                 'Client': f.get('rm.client'),
                                 'Parent_Ref': f.get('xip.parent_ref'),
-                                'Cover_Dates': f.get('rm.coverdates'),
-                                'Status_Date': f.get('rm.statusdate')
+                                'Cover_Dates': f.get('rm.coverdate'),
+                                'Status_Date': datetime.fromtimestamp(f.get('rm.statusdate')/1000)                                
                                 }
-                assignments = retention.assignments(asset)
+                assignments = self.retention.assignments(asset)
                 if len(assignments) > 1: print(f"\n Number of Assignments for {f.get('xip.reference')} is: " + str(len(assignments)))
+                policy = self.retention.policy(reference=f.get('xip.retention_policy_assignment_ref')[0]) 
                 if self.expired_flag:
-                    self.report_expired(assignments,dict_retention)
+                    self.report_expired(assignments,policy,dict_retention)
                 else:
-                    policy = retention.policy(reference=f.get('xip.retention_policy_assignment_ref')[0]) 
                     self.report_not_expired(assignments,policy,dict_retention)
                 list_retention.append(dict_retention)
             df = pd.DataFrame.from_records(list_retention)
+            print()
             if self.expired_flag: df = df.dropna(subset=['Expired'])
             if self.path_flag:
                 list_path = df[['Reference','Document_Type']].values.tolist()
                 list_dict_path = []
-                dict_path = {}
                 t = len(list_path)
                 c = 0
-                for record in list_path:
+                for r in list_path:
+                    dict_path = {}
                     c += 1
                     print(f"Retrieving Path: {c} / {t}", end="\r")
-                    path,parent_name = path_return(ref=record[0],io=record[1],split_flag=self.split_flag)
-                    dict_path.update({'Parent_Name':parent_name,'Path':path})
+                    path,parent_name,parent_desc = self.path_return(ref=r[0],io=r[1],split_flag=self.split_flag)
+                    dict_path.update({'Reference':r[0],'Parent_Name':parent_name,'Parent_Desc': parent_desc,'Path':path})
                     list_dict_path.append(dict_path)
                 dfpath = pd.DataFrame.from_records(list_dict_path)
-                df.merge(dfpath,left_index=True,right_index=True,how="left")
-            boxdf = df.drop(columns=['Reference','Title','Description'])
-            boxdf = boxdf.drop_duplicates(subset=['Parent_Ref'])
-            boxdf = boxdf.rename({"Parent_Ref":"Reference","Parent_Title":"Title"},axis='columns')
+                df = df.merge(dfpath,on="Reference",how="left")
+            print()
+            boxdf = df.drop_duplicates(subset=['Parent_Ref'])
+            if not self.path_flag:
+                boxdf.reset_index(drop=True)
+                boxdf['Parent_Name'] = boxdf['Title'].str.rsplit("/",n=1).str[0]
+            else:
+                boxdf['Path'] = boxdf['Path'].str.rsplit(":::",n=1).str[0]
+            boxdf = boxdf.drop(columns=['Reference','Title','Description','Cover_Dates'])                
+            boxdf = boxdf.rename({"Parent_Ref":"Reference","Parent_Name":"Title","Parent_Desc":"Description"},axis='columns')                    
             boxdf['Flag'] = "Box"
             df['Flag'] = "Item"
-            df = pd.concat([df,boxdf])
-            df.sort_values(by=['Title'])
+            df = pd.concat([df,boxdf],ignore_index=True)
+
+            #df['SortHelper'] = df['Title'].str.split('/',expand=True).fillna(value="0")
+            df['SortHelper'] = df['Title'].apply(lambda x: "-".join([str(x).zfill(5) for x in str(x).split("/")]))
+            df = df.sort_values(by=['SortHelper'])
+            df = df.drop(columns='SortHelper')
             if self.report_mode == "*": report_title = "ALL"
             else: report_title = self.report_mode
             output_file = os.path.join(self.output_dir,f'Retention Report_{report_title + "_" + str(datetime.now().strftime("%Y-%m-%d"))}.xlsx')
-            with pd.ExcelWriter(output_file,engine='auto',mode='w') as writer:
-                df.to_excel(writer,index=False,sheet_name="Master")
-                boxdf.to_excel(writer,index=False,sheet_name="Box Summary")
-            print(f'File Saved to: {output_file}')
+            self.export_xl(df,boxdf,output_file)
+                
         else: print('No Results Returned...')
         
 if __name__ == '__main__':
-
     startTime = datetime.now()
     print(f"Start time: {str(startTime)}")
-    
-    ref = "759c51a1-d20c-4b16-b08b-dd3b2719b682"
-    output_dir = r"C:\Users\Chris.Prince\Unilever\UARM Teamsite - Archive\Digital Preservation\github\presRM_tools\presRM_reports"
-    RetentionReport(REF=ref,REPORT_TYPE="Archive",PATH_FLAG=True,OUTPUT_DIR=output_dir).main()
+    entity,retention,content = login_preservica(user=secret.username,passwd=secret.password)
+    ref = "2591783b-725e-4373-9ac1-64e73541e7fa"
+    #output_dir = r"C:\Users\Chris.Prince\Unilever\UARM Teamsite - Archive\Digital Preservation\github\presRM_tools\presRM_reports"
+    RetentionReport(entity=entity,retention=retention,content=content,REF=ref,REPORT_TYPE="All",EXPIRED_FLAG=True,PATH_FLAG=False).main()
     print(f"Complete, running time: {datetime.now() - startTime}")
--- presRM_reports/presRM_retention_prompt.py
+++ presRM_reports/presRM_retention_prompt.py
@@ -1,13 +1,54 @@
-from presRM_rentention import RetentionReport
+from presRM_rentention import login_preservica,RetentionReport
 from time import sleep
 import os
+from getpass import getpass
+try: 
+    import secret
+    s_flag = True
+except ImportError:
+    s_flag = False
+
+list_of_useful_refs = [["Records Management UK", "bf614a5d-10db-4ff8-addb-2c913b3149eb"],
+["Signed Off Records Management UK",	"3cea8f86-1f4e-43dc-81c3-cfa81d5370f0"],
+["TM Database", "be7aeb3a-8874-4067-9b62-b46427df8c1d"],
+["USCC",	"50de12a0-1466-4d18-9c1a-547450c5579d"],
+["Records Management NL", "5f76e749-ddf3-4d3d-a31c-bd07864e9dc2"],
+["UK [Root Folder]", "2591783b-725e-4373-9ac1-64e73541e7fa"],
+["NL [Root Folder]", "ca45be7a-4a92-4732-8168-4d0f3cef26a2"]]
+
 
 def raise_invalid():
     print('Invalid Option... Raising SystemExit...')
     sleep(2)
     raise SystemExit()
+
+def enter_signin(fail_count=0):
+    if s_flag:
+        print(f'Secret file detected, utilising credentials for: {secret.username}')
+        user = secret.username
+        passwd = secret.password
+    else:
+        user = input("""Please enter your Preservica Username: """)
+        passwd = getpass("""Please enter your Preservica Password: """)
+    try:
+        entity,retention,content = login_preservica(user=user,passwd=passwd)
+    except Exception as e:
+        if e.args[0] in {401}:    
+            print('Error signing in... Please try again...')
+            if fail_count == 2: print('This is your last chance...')
+            elif fail_count == 3: print('YOU ARE DONE'); raise SystemExit()
+            fail_count += 1
+            entity, retention, content = enter_signin(fail_count)
+        elif e.args[0] in {404,501}:
+            print('Preservica is having a moment, please try again later')
+        else:
+            print('An unknown error occured... Panic!')
+            print(e)
+            sleep(5)        
+    return entity,retention,content
+
 def enter_inputs():
-    ref_input = input(r"""
+    print(r"""
 Please enter the Preservica Reference of the folder you want to search
 
 *** To use the default settings, simply hit enter, this applies for all options ***
@@ -15,8 +56,15 @@ Please enter the Preservica Reference of the folder you want to search
 
 - This searches the selected folder and all subfolders
 - You can select also select folder reference on 'lower levels', for limited searches / testing purposes
-- NL folder reference is:
-
+          
+Please see the below for a list of useful references
+""")
+    headers = ['Folder','Reference']
+    print('{:<50s} {:<30s}'.format(*headers))
+    for _list in list_of_useful_refs:
+        print('{:<50s} {:<30s}'.format(*_list))
+    ref_input = input(r"""
+                    
 Enter reference: """)
     
     if not ref_input:
@@ -98,8 +146,8 @@ Please select if you want to remove Prefix and Suffixes from the Path.
 
 Enter Y / N: """)
         split_input = split_input.upper()
-        if split_input in {"Y", "YES",True}: split_input = True; print('Not running prefix and suffix removal on Paths')
-        elif split_input in {"N","NO",False}: split_input = False; print('Running prefix and suffix on Paths')
+        if split_input in {"Y", "YES",True}: split_input = True; print('Running prefix and suffix removal on Paths')
+        elif split_input in {"N","NO",False}: split_input = False; print('Not running prefix and suffix on Paths')
         elif not split_input: split_input = True; print("Using Default: Y")
         else: print('Invalid option, defaulting to False'); split_input = False 
     elif path_input in {"N","No", False}: path_input = False; split_input = False; ('Not running Path Export')
@@ -109,8 +157,9 @@ Enter Y / N: """)
     return ref_input,directory_input,report_input,expired_input,path_input,split_input
 
 def main():
+    entity,retention,content = enter_signin()
     ref_input,directory_input,report_input,expired_input,path_input,split_input = enter_inputs()    
-    RetentionReport(REF=ref_input,OUTPUT_DIR=directory_input,REPORT_TYPE=report_input,EXPIRED_FLAG=expired_input,PATH_FLAG=path_input,SPLIT_FLAG=split_input).main()
+    RetentionReport(entity=entity,retention=retention,content=content,REF=ref_input,OUTPUT_DIR=directory_input,REPORT_TYPE=report_input,EXPIRED_FLAG=expired_input,PATH_FLAG=path_input,SPLIT_FLAG=split_input).main()
     
 if __name__ == "__main__":
     main()
